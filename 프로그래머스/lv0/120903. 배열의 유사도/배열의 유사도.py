71e0d25ebb786f1221e8ee6647037b8bfb641e57
kngsujng <kngsujng@naver.com>
2023-07-25 12:15:13 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,2 +1,7 @@
 def solution(s1, s2):
-    return len(set(s1) & set(s2))
+    arr=[]
+    for i in s1:
+        for j in s2:
+            if i==j:
+                arr.append(i)
+    return len(arr)
